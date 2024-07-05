d3a3329d8cfb762319b9dfda4edc50b330ccf3ae
tube1008 <ilhyuk0504@gmail.com>
2024-07-05 17:39:28 +0900

    작업물 만드는중

--- script/script.js
+++ script/script.js
@@ -16,7 +16,7 @@ let swiper = new Swiper('#projectContainer', {
     // 한 번에 보여줄 슬라이드 수
     slidesPerView: 1,
     // 슬라이드 사이의 간격
-    spaceBetween: 80,
+    spaceBetween: 1000,
     // 한 번에 넘길 슬라이드 수
     slidesPerGroup: 1,
     centeredSlides : true,
@@ -51,3 +51,41 @@ let swiper03 = new Swiper('#workContainer2', {
     slidesPerGroup: 1,
     centeredSlides : true,
 })
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
